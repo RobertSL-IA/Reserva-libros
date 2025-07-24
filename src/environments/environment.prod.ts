0e667ce12bf5758d1e70b32d0e9c5474babc117f
RobertSL07 <robertsulcalazo07@outlook.com>
2025-07-24 02:27:29 -0500

    Cambios environment HTTPS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://18.229.137.205:8080/api'
+  apiUrl: 'https://18.229.137.205:8443/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://18.229.137.205:8443/api'
 };
