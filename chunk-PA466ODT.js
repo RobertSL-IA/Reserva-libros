a49b2f1112d44b17ceb768b1b485a6039770d369
RobertSL07 <robertsulcalazo07@outlook.com>
2025-07-23 23:59:39 -0500

    Auto-generated commit

---
+++ chunk-PA466ODT.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chunk-3WNAHTTH.js";import"./chunk-ZGKWI5GO.js";export{a as Prestamo};
---
+++ chunk-TCRTASDM.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-TG2XIHNQ.js";import"./chunk-ZGKWI5GO.js";export{a as Detalle};
